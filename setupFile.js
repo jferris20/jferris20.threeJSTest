32ba77102348e51be1e9209e3cb9e0d6e1d3bf51
jfarris2 <jpferris2026@gmail.com>
2020-06-04 20:57:27 -0600

    Attempt whatever
    Moved some stuff around to try to make the external js file work

--- setupFile.js
+++ setupFile.js
@@ -1,16 +1,23 @@
 // Set up GUI
-function setGUI() {
 
-	container = document.getElementById( 'container' );
-					
-	guiControls = new function() {
-		this.rotationX = 0.00;
-		this.rotationY = 0.00;
-		this.rotationZ = 0.00;
+<script type="module">
+	import { GUI } from '/gui/dat.gui.module.js';
+
+	var guiControls, datGUI;
+
+	function setGUI() {
+
+		container = document.getElementById( 'container' );
+						
+		guiControls = new function() {
+			this.rotationX = 0.00;
+			this.rotationY = 0.00;
+			this.rotationZ = 0.00;
+		}
+
+		datGUI = new GUI();
+		datGUI.add(guiControls, 'rotationX', 0, 360);
+		datGUI.add(guiControls, 'rotationY', 0, 360);
+		datGUI.add(guiControls, 'rotationZ', 0, 360);
 	}
-
-	datGUI = new GUI();
-	datGUI.add(guiControls, 'rotationX', 0, 360);
-	datGUI.add(guiControls, 'rotationY', 0, 360);
-	datGUI.add(guiControls, 'rotationZ', 0, 360);
-}
+</script>
