39cfc149d62d3bf43abb4ece31e945bad3fcdf7b
jfarris2 <jpferris2026@gmail.com>
2020-06-06 21:16:06 -0600

    Removed unused code
    I moved code I wasn't currently using or testing into a seperate file. I also adjusted the camera.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 // Import stuff
 import * as THREE from '../js/three.module.js';
 import { OrbitControls } from '../controls/OrbitControls.js';
-import { GUI } from '../gui/dat.gui.module.js';
 import { GLTFLoader } from '../loaders/GLTFLoader.js';
 import { STLExporter } from '../exporters/STLExporter.js';
 
 
 // Set up variables
-var camera, controls, scene, renderer, cube, container;
-var guiControls, datGUI;
+var camera, controls, scene, renderer, container;
 var hemiLight, dirLight;
 var exporter;
 var model;
@@ -25,22 +23,10 @@ function init() {
 	// Container for UI 
 	container = document.getElementById( 'container' );
 	
-	guiControls = new function() {
-		this.rotationX = 0.00;
-		this.rotationY = 0.00;
-		this.rotationZ = 0.00;
-	}
-	
-	datGUI = new GUI();
-	datGUI.add(guiControls, 'rotationX', 0, 360);
-	datGUI.add(guiControls, 'rotationY', 0, 360);
-	datGUI.add(guiControls, 'rotationZ', 0, 360);
-	
 	// Set up exporter
 	exporter = new STLExporter();
 	var buttonExportBinary = document.getElementById( 'exportBinary' );
 	buttonExportBinary.addEventListener( 'click', exportBinary );
-	
 	
 	// Create the scene
 	scene = new THREE.Scene();
@@ -55,31 +41,26 @@ function init() {
 
 	// Create a camera
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.set( 3, 3, 3 );
-	camera.lookAt( 0, 1, 0 );
+	camera.position.set( 20, 20, 20 );
+	camera.lookAt( 0, 18, 0 );
 	
 	// Controls
 	controls = new OrbitControls( camera, renderer.domElement );
-	//controls.screenSpacePanning = true;
+	controls.screenSpacePanning = true;
 	controls.enablePan = false;
-	controls.minDistance = 3;
-	controls.maxDistance = 30;
+	controls.minDistance = 15;
+	controls.maxDistance = 40;
 	controls.target.set( 0, 2, 0 );
-	//controls.maxPolarAngle = Math.PI * 0.495; //Hopefully restricts vertical camera rotations. Needs further adjustment
+	controls.maxPolarAngle = Math.PI * 0.495; //Hopefully restricts vertical camera rotations. Needs further adjustment
 	
 	controls.update();
 
 	// Add lights
 	hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
-	//hemiLight.color.setHSL( 0.6, 1, 0.6 );
-	//hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
 	hemiLight.position.set( 0, 50, 0 );
 	scene.add( hemiLight ); 
-
-
 	
-	dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-	//dirLight.color.setHSL( 0.1, 1, 0.95 );
+	dirLight = new THREE.DirectionalLight( 0xffffff, 1 ); 
 	dirLight.position.set( - 1, 1.75, 1 );
 	dirLight.position.multiplyScalar( 30 );
 	scene.add( dirLight );
@@ -98,21 +79,8 @@ function init() {
 
 	dirLight.shadow.camera.far = 3500;
 	dirLight.shadow.bias = - 0.0001;
-
-	// Load a texture for the cube
-	var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-
-	// Create a cube
-	var geometry = new THREE.BoxGeometry();
-	//var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-	var material = new THREE.MeshBasicMaterial( { map: texture } );
-	cube = new THREE.Mesh( geometry, material );
-	cube.position.set(0, 1, 0);
-	scene.add( cube );
-	
 	
 	// Import object using GLFT loader
-	// Used example code from https://github.com/mrdoob/three.js/blob/master/examples/webgl_animation_skinning_additive_blending.html
 	var loader = new GLTFLoader();
 	loader.load( 'gltfModels/GhostPirate.glb', function ( gltf ) {
 
@@ -129,55 +97,32 @@ function init() {
 	var material = new THREE.LineBasicMaterial( { color: 0x0000ff } ); // Blue
 	var points = [];
 	points.push( new THREE.Vector3( 0, 0, 0 ) );
-	points.push( new THREE.Vector3( 0, 3, 0 ) ); // Y Axis
+	points.push( new THREE.Vector3( 0, 10, 0 ) ); // Y Axis
 	var geometry = new THREE.BufferGeometry().setFromPoints( points );
 	var line = new THREE.Line( geometry, material );
 	scene.add( line );
 
 	material = new THREE.LineBasicMaterial( { color: 0x00ff00 } ); // Green
 	points.pop();
-	points.push( new THREE.Vector3( 0, 0, 3 ) ); // Z Axis
+	points.push( new THREE.Vector3( 0, 0, 10 ) ); // Z Axis
 	geometry = new THREE.BufferGeometry().setFromPoints( points );
 	line = new THREE.Line( geometry, material );
 	scene.add( line );
 	
 	material = new THREE.LineBasicMaterial( { color: 0xff0000 } ); // Red
 	points.pop();
-	points.push( new THREE.Vector3( 3, 0, 0 ) ); // X Axis
+	points.push( new THREE.Vector3( 10, 0, 0 ) ); // X Axis
 	geometry = new THREE.BufferGeometry().setFromPoints( points );
 	line = new THREE.Line( geometry, material );
 	scene.add( line );
 
 	createGround();
-	
-	// Attempt to merge different meshes/geometries
-	/*var ballGeo = new THREE.SphereGeometry(10,35,35);
-	var material = new THREE.MeshPhongMaterial({color: 0xF7FE2E}); 
-	var ball = new THREE.Mesh(ballGeo, material);
-
-	var pendulumGeo = new THREE.CylinderGeometry(1, 1, 50, 16);
-	ball.updateMatrix();
-	pendulumGeo.merge(ball.geometry, ball.matrix);
-
-	pendulum = new THREE.Mesh(pendulumGeo, material);
-	scene.add(pendulum);*/ //It worked
-
 }
-			
-// function onWindowResize() {}			
-/*function onWindowResize() {
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
-
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-
-	//render();
-}*/
 
 function exportBinary() {
-	var result = exporter.parse( cube, { binary: true } );
-	saveArrayBuffer( result, 'test1.stl' );
+	var result = exporter.parse( model, { binary: true } );
+	saveArrayBuffer( result, 'test2.stl' );
 }
 
 var link = document.createElement( 'a' ); // Not 100% certain what this does
@@ -196,9 +141,7 @@ function saveArrayBuffer( buffer, filename ) {
 
 // Animation loop
 function animate() {
-	cube.rotation.x = guiControls.rotationX * (Math.PI / 180);
-	cube.rotation.y = guiControls.rotationY * (Math.PI / 180);
-	cube.rotation.z = guiControls.rotationZ * (Math.PI / 180);
+	
 
 	requestAnimationFrame( animate );
 	
